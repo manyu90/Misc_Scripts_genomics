7ba71643d41173a963241268577f93a923b6a551
manyu90 <manyu@stanford.edu>
2017-09-15 16:58:20 -0700

    Fixed bugs related to non existing intervals in the Fasta

--- analyse_chip_seq_data.py
+++ analyse_chip_seq_data.py
@@ -22,7 +22,10 @@ class ChipSeq_file_methylation(object):
 		self.methylation_interval_dict=defaultdict(float)
 		self.fasta_file=FastaFile('/mnt/data/annotations/by_organism/human/hg20.GRCh38/GRCh38.genome.fa')
 		self.bw_filtered=pyBigWig.open('/mnt/lab_data/kundaje/manyu/Methylation_data/K562_wgbs_filtered.bigWig')
-
+		self.chrom_names_path='/mnt/data/annotations/by_release/hg20.GRCh38/chrom.names_short'
+		with open(self.chrom_names_path,'rb') as f:
+			self.chrom_names=[line.strip('\n') for line in f]
+	#	print (len(self.chrom_names),self.chrom_names[0:5])
 		try:
 			if re.search('bed.gz', self.path):
 				with gzip.open(path,'rb') as f:
@@ -54,23 +57,30 @@ class ChipSeq_file_methylation(object):
 	def methylation_percentage(self,region,start,end):
 	    fasta_file=self.fasta_file
 	    bw_filtered=self.bw_filtered
-	    sequence=fasta_file.fetch(region,start,end+1).lower()
-	    #print(sequence)
-	    count_gc=0.0
-	    sum_gc=0.0
-	    for count in xrange(len(sequence)):
-	        if self.find_CpG(sequence,count):
-	            count_gc+=1
-	    data_methylation=bw_filtered.values(region,start,end+1,numpy=True)
-	    nan_to_zero(data_methylation)
-	    sum_gc=np.sum(data_methylation)
-	    if count_gc>0:
-	        # print("Returning total CpG,Fraction of methylated Cpg,Fraction of Methylated sites \n")
-	        # print("#CpG,Total Methylation/#CpG,Total Methylation/Total Sites \n")
-	        return count_gc,sum_gc/count_gc,sum_gc/len(sequence)
-	    
-	    else:
-	        return 0,0.0,0.0
+	    try:
+	    	
+		    sequence=fasta_file.fetch(region,start,end+1).lower()
+		    	
+
+		    #print(sequence)
+		    count_gc=0.0
+		    sum_gc=0.0
+		    for count in xrange(len(sequence)):
+		        if self.find_CpG(sequence,count):
+		            count_gc+=1
+		    data_methylation=bw_filtered.values(region,start,end+1,numpy=True)
+		    nan_to_zero(data_methylation)
+		    sum_gc=np.sum(data_methylation)
+		    if count_gc>0:
+		        # print("Returning total CpG,Fraction of methylated Cpg,Fraction of Methylated sites \n")
+		        # print("#CpG,Total Methylation/#CpG,Total Methylation/Total Sites \n")
+		        return count_gc,sum_gc/float(count_gc),sum_gc/float(len(sequence))
+		    
+		    else:
+		        return 0,0.0,0.0
+	    except:
+	        pass
+		        
 
 
 	
@@ -78,14 +88,17 @@ class ChipSeq_file_methylation(object):
 	def calculate_methylation(self):
 		for interval in self.data_lines:
 			region,start,end =interval[0],int(interval[1]),int(interval[2])
-			self.methylation_interval_dict[interval]=self.methylation_percentage(region,start,end)
+			if region in self.chrom_names:
+				self.methylation_interval_dict[(region,start,end)]=self.methylation_percentage(region,start,end)
+			else:
+				continue
 			#print "interval \t %s"%(str(interval))
 
 
 
 					
 	def write_to_file(self):
-		self.calculate_methylation()  #Calculating meylation, populating the dictionary
+                self.calculate_methylation()  #Calculating meylation, populating the dictionary
 		methylation_data_list=[]  #List version of the methylation_dict
 		#print len(methylation_data_list)
 		for key in self.methylation_interval_dict:
@@ -104,11 +117,15 @@ class ChipSeq_file_methylation(object):
 
 
 if __name__=='__main__':
-	path='/srv/scratch/manyu/NIPS_workshop_tests/test.bed'
+	#path='/srv/scratch/manyu/NIPS_workshop_tests/test.bed'
 	#path='/mnt/lab_data/kundaje/manyu/ChipSeq/CEBPB_narrowPeak_peaks_mainChrom.bed'
-	path_to_write='/srv/scratch/manyu/NIPS_workshop_tests/CEBPB_methylation_sorted_test'
-	ChipSeq_file_methylation(path, path_to_write).write_to_file()
-	
+	path='/srv/scratch/manyu/data/hg38/K562/ENCSR000FAU/released/GRCh38/optimal_idr_thresholded_peaks/bed/narrowpeak/rep1_rep2/ENCFF747ICD.bed.gz'
+	#path_to_write='/srv/scratch/manyu/NIPS_workshop_tests/CEBPB_methylation_sorted_test'
+	path_to_write='/srv/scratch/manyu/NIPS_workshop_tests/test_gzip_ENCSR000FAU'
+	#ChipSeq_file_methylation(path, path_to_write).write_to_file()
+        path='/srv/scratch/manyu/data/hg38/K562/ENCSR000BKH/released/GRCh38/optimal_idr_thresholded_peaks/bed/narrowpeak/rep1_rep2/ENCFF496YJC.bed.gz'
+        path_to_write='/srv/scratch/manyu/NIPS_workshop_tests/test_gzip_ENCSR000BKH'
+        ChipSeq_file_methylation(path, path_to_write).write_to_file()	
 
 
 
